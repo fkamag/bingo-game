0ab0cda8a533ff09614fbdea718317242ac16110
fkamag <fabiokamagian@gmail.com>
2023-11-14 21:46:34 -0300

    scanner players

--- src/Bingo.java
+++ src/Bingo.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bingo {
@@ -10,6 +11,13 @@ public class Bingo {
     System.out.println("      B I N G O     ");
     System.out.println("--------------------");
     System.out.println();
+    System.out.println("Digite o nome dos participantes separados por '-'"
+        + " e sem espaço: ");
+    String scannerPlayer = scanner.next();
+    System.out.println();
+    String[] players = scannerPlayer.split("-");
+
+    System.out.println(Arrays.deepToString(players));
   }
 
 }
