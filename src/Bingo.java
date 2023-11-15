a21fabba5e8bec47f2397f8d5be9dd2edbc945ed
fkamag <fabiokamagian@gmail.com>
2023-11-14 21:16:28 -0300

    iniciando tela inicial

--- src/Bingo.java
+++ src/Bingo.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Bingo {
 
   public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("--------------------");
+    System.out.println("      B I N G O     ");
+    System.out.println("--------------------");
+    System.out.println();
   }
 
 }
