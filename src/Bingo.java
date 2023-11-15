0064d89511f57341852427cda5e48b1aff0e8842
fkamag <fabiokamagian@gmail.com>
2023-11-14 22:02:07 -0300

    feat gerar cartelas manualmente

--- src/Bingo.java
+++ src/Bingo.java
@@ -17,14 +17,44 @@ public class Bingo {
     System.out.println();
     String[] players = scannerPlayer.split("-");
 
-    System.out.println(Arrays.deepToString(players));
-
     String option = "";
     while (!option.equals("1") && !option.equals("2")) {
       System.out.println("Escolha a opção para gerar as cartelas");
       System.out.println("1 - Cartelas Automáticas");
       System.out.println("2 - Cartelas Manuais");
       option = scanner.next();
+
+      String cartela;
+
+      if (option.equals("1")) {
+        System.out.println("Gerando as cartelas automaticamente...");
+      } else {
+        System.out.println("Digite as cartelas utilizando o formato a seguir:");
+        System.out.println("1,2,3,4,5-6,7,8,9,1-2,3,4,5,6");
+        System.out.println("Seguindo a ordem dos participantes a baixo:");
+        for (String p : players) {
+          System.out.print(p + ", ");
+        }
+        System.out.println();
+        cartela = scanner.next();
+
+        String[] cartelas = cartela.split("-");
+        int [][] cartelasPorJogador = new int[players.length][5];
+
+        String[] nova;
+        int number;
+        for (int linha=0; linha< players.length; linha++) {
+          nova = cartelas[linha].split(",");
+          for (int coluna=0; coluna< 5; coluna++) {
+            number = Integer.parseInt(nova[coluna]);
+            cartelasPorJogador[linha][coluna] = number;
+          }
+        }
+
+        System.out.println(Arrays.deepToString(players));
+        System.out.println(Arrays.deepToString(cartelasPorJogador));
+      }
+
     }
 
   }
