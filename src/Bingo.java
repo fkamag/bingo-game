c7a1d547ed0ec6716857bb72debc44533a13cfcd
fkamag <fabiokamagian@gmail.com>
2023-11-15 20:58:05 -0300

    feat gerar cartelas automaticamente

--- src/Bingo.java
+++ src/Bingo.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Bingo {
@@ -23,40 +24,57 @@ public class Bingo {
       System.out.println("1 - Cartelas Automáticas");
       System.out.println("2 - Cartelas Manuais");
       option = scanner.next();
-
-      String cartela;
-
-      if (option.equals("1")) {
-        System.out.println("Gerando as cartelas automaticamente...");
-      } else {
-        System.out.println("Digite as cartelas utilizando o formato a seguir:");
-        System.out.println("1,2,3,4,5-6,7,8,9,1-2,3,4,5,6");
-        System.out.println("Seguindo a ordem dos participantes a baixo:");
-        for (String p : players) {
-          System.out.print(p + ", ");
-        }
-        System.out.println();
-        cartela = scanner.next();
-
-        String[] cartelas = cartela.split("-");
-        int [][] cartelasPorJogador = new int[players.length][5];
-
-        String[] nova;
-        int number;
-        for (int linha=0; linha< players.length; linha++) {
-          nova = cartelas[linha].split(",");
-          for (int coluna=0; coluna< 5; coluna++) {
-            number = Integer.parseInt(nova[coluna]);
-            cartelasPorJogador[linha][coluna] = number;
-          }
-        }
-
-        System.out.println(Arrays.deepToString(players));
-        System.out.println(Arrays.deepToString(cartelasPorJogador));
-      }
-
     }
 
+    String cartela;
+    int [][] cartelasPorJogador = new int[players.length][5];
+    int number;
+    Random r = new Random();
+
+    if (option.equals("1")) {
+      for (int i = 0; i < players.length; i++) {
+        for (int j = 0; j < 5; j++) {
+          number = r.nextInt(60)+1;
+          while (true) {
+            boolean isDuplicate = false;
+            for (int k = 0; k < 5; k++) {
+              if (cartelasPorJogador[i][k] == number) {
+                isDuplicate = true;
+                break;
+              }
+            }
+            if (isDuplicate) {
+              number = r.nextInt(60)+1;
+            } else {
+              break;
+            }
+          }
+          cartelasPorJogador[i][j] = number;
+        }
+      }
+    } else {
+      System.out.println("Digite as cartelas utilizando o formato a seguir:");
+      System.out.println("1,2,3,4,5-6,7,8,9,1-2,3,4,5,6");
+      System.out.println("Seguindo a ordem dos participantes a baixo:");
+      for (String p : players) {
+        System.out.print(p + ", ");
+      }
+      System.out.println();
+      cartela = scanner.next();
+
+      String[] cartelas = cartela.split("-");
+
+      String[] nova;
+      for (int linha=0; linha< players.length; linha++) {
+        nova = cartelas[linha].split(",");
+        for (int coluna=0; coluna< 5; coluna++) {
+          number = Integer.parseInt(nova[coluna]);
+          cartelasPorJogador[linha][coluna] = number;
+        }
+      }
+    }
+    System.out.println(Arrays.deepToString(players));
+    System.out.println(Arrays.deepToString(cartelasPorJogador));
   }
 
 }
